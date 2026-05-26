ce2671758fcd6dccb283ab827507f0deb4bb0e13
gusthsb <gustavo.hdsbrito@gmail.com>
2026-05-26 18:32:10 -0300

    Add files via upload

---
+++ libft/ft_memcpy.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gustde-s <gustde-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/21 15:51:42 by gustde-s          #+#    #+#             */
+/*   Updated: 2026/05/25 15:20:55 by gustde-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t				i;
+	unsigned char		*d;
+	const unsigned char	*s;
+
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	i = 0;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dest);
+}
+
+// int	main(void)
+// {
+// 	char	src[40] = "Olá, mundo";
+// 	char	dest[40];
+
+// 	ft_memcpy(dest, src, 12);
+// 	printf("Dest = %s\n", dest);
+// 	return (0);
+// }
